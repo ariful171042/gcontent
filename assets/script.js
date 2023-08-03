adb7517702b0c0a53d0e22b5c357f87cb2e42d26
ariful171042 <arif171042@gmail.com>
2023-08-03 18:41:01 +0600

    res

--- assets/script.js
+++ assets/script.js
@@ -83,3 +83,19 @@ document.addEventListener("keydown", function (event) {
   event.key === "ArrowLeft" && previousSlide();
   event.key === "ArrowRight" && nextSlide();
 });
+
+toggleBtn.addEventListener("click", function (event) {
+  if (navLinks.classList.contains("nav__open")) {
+    navLinks.classList.remove("nav__open");
+    document.querySelector("html").style.overflow = "visible";
+  } else {
+    navLinks.classList.add("nav__open");
+    document.querySelector("html").style.overflow = "hidden";
+  }
+});
+
+navLinks.addEventListener("click", function () {
+  navLinks.classList.contains("nav__open") &&
+    navLinks.classList.remove("nav__open");
+  document.querySelector("html").style.overflow = "visible";
+});
